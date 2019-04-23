b96ef22cda6d1c0175f50a9937f70017a7d40536
zxn <446827345@qq.com>
2019-04-23 23:05:06 +0800

    后台首页面创建完毕

--- src/main.js
+++ src/main.js
@@ -17,6 +17,23 @@ Vue.use(ElementUI)
 
 // axios配置请求公共根地址
 axios.defaults.baseURL = 'http://127.0.0.1:11333/api/private/v1/'
+
+// 添加请求拦截器
+axios.interceptors.request.use(
+  config => {
+    // Do something before request is sent
+    // return config;
+    // console.dir(config)
+    var token = window.sessionStorage.getItem('token')
+    config.headers.Authorization = token
+    return config
+  },
+  error => {
+    // Do something with request error
+    return Promise.reject(error)
+  }
+)
+
 // 给axios配置成为Vue的成员
 Vue.prototype.$http = axios
 
--- src/router/index.js
+++ src/router/index.js
@@ -5,13 +5,20 @@ import Router from 'vue-router'
 // 引入Login.vue组件
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '@/components/Welcome'
 
 Vue.use(Router)
 
 const router = new Router({
   routes: [
+    { path: '/', redirect: '/home' },
     { path: '/login', component: Login },
-    { path: '/home', component: Home }
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [{ path: '/welcome', component: Welcome }]
+    }
   ]
 })
 
